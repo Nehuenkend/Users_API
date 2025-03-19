cc98f39e8397e7c24caf42bd0726a5cf0f02c789
Nehuen Kendziura <121322725+Nehuenkend@users.noreply.github.com>
2025-03-19 17:20:37 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
-from routers import users
+from routers import users, jwt_auth_users, users_db
 
 app = FastAPI()
 
 app.include_router(users.router)
+app.include_router(jwt_auth_users.router)
+app.include_router(users_db.router)
+
+@app.get("/items/{item_id}")
+def read_items(item_id):
+    return {"item_id": item_id}
